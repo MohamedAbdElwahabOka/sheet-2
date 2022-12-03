98f7dda3b14ed2f8696587dc1ea07ea8b7ad4f41
Mohamed Abd Elwahab Elazab Oka <93409090+MohamedAbdElwahabOka@users.noreply.github.com>
2022-12-03 07:55:52 +0200

    Create D. Fixed Password.cpp

---
+++ D. Fixed Password.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    
+    while(true){
+        cin>>n;
+        
+        if(n == 1999){
+            cout<<"Correct"<<"\n";
+            return 0;
+        }else{
+            cout<<"Wrong"<<"\n";
+        }
+    }
+   
+    
+    return 0;
+}
